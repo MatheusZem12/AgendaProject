216db15c9632c2866a2fb079a760ed1f18ae723c
Matheus Zem <matheuslajazem@gmail.com>
2025-02-05 09:20:17 -0300

    feature: Creating Category

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from contact.models import Contact, Category
 
 # Register your models here.
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = 'id', 'name', 'description', 'show',
+    ordering = 'id',
+    search_fields = 'name', 'description',
+    list_filter = 'show',
+    list_per_page = 10
+    list_max_show_all = 100
+
+@admin.register(Contact)
+class AdminContact(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 'created_date', 'description', 'show', 'picture',
+    ordering = 'id',
+    search_fields = 'first_name', 'last_name', 'email',
+    list_filter = 'show', 'created_date',
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = 'show',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,36 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    show = models.BooleanField(default=True)
+    
+    def __str__(self) -> str:
+        return f'{self.id} - {self.name}'
+    
+    class Meta():
+        db_table = 'tb_category'
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=12)
+    email = models.EmailField(max_length=122)
+    created_date = models.DateField(default=timezone.now)
+    description = models.TextField()
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return f'{self.id} - {self.first_name} {self.last_name}'
+    
+    class Meta():
+        db_table = 'tb_contact'
