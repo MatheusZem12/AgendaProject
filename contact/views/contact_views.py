f881a9cf91c668364e9c7643463872f2d818f561
Matheus Zem <matheuslajazem@gmail.com>
2025-02-06 11:28:42 -0300

    fix: Changing the size of Contact's phone and the response of Contact table view

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.all().order_by('id').filter(show=True)
     return render(request, 'contact/index.html', context = {'contacts': contacts})
