9b64ac540cdc775f441b80b18546f4e080135f3e
Matheus Zem <matheuslajazem@gmail.com>
2025-02-07 15:01:29 -0300

    feature: Creating paginator to contact

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import redirect, render, get_object_or_404, get_list_or_404
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 def find_all_contact(request):
-    contacts = Contact.objects.order_by('id').filter(show=True)
-    return render(request, 'contact/index.html', context = {'contacts': contacts})
+    contacts = Contact.objects.filter(show=True).order_by('id')
+    paginator = Paginator(contacts, 10)
+    page = request.GET.get('page')
+    page_obj = paginator.get_page(page)
+    return render(request, 'contact/index.html', context = {'page_obj': page_obj})
 
 def search_contact(request):
     search = request.GET.get('q','')
     if search:
         redirect('contact:index')
-    print(search)
     contacts = Contact.objects.filter(
         Q(first_name__icontains=search) |
         Q(id__icontains=search)
     )
-    print(contacts.query)
-    return render(request, 'contact/index.html', context = {'contacts': contacts})
+    paginator = Paginator(contacts, 10)
+    page = request.GET.get('page')
+    page_obj = paginator.get_page(page)
+    return render(request, 'contact/index.html', context = {'page_obj': page_obj})
 
 
 def find_contact_by_id(request, contact_id):
